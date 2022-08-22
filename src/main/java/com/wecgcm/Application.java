f39dc4ea01a0178ac9d1e602d9eff7ae45de8f93
wecgwm <wecgwm>
2022-08-23 01:10:16 +0800

    test:test docker in ci

--- src/main/java/com/wecgcm/Application.java
+++ src/main/java/com/wecgcm/Application.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

89bcce48ee4057ad49e2bec9702da6003140d32f
wecgwm <wecgwm>
2023-07-12 21:28:40 +0800

    fix: context path

--- src/main/java/com/wecgcm/Application.java
+++ src/main/java/com/wecgcm/Application.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("${spring.application.name}")
 @RequiredArgsConstructor(onConstructor_= @Autowired)
 public class Application {
     private final DemoConfiguration demoConfiguration;

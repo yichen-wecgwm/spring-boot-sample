649fb850eff65151c09e1dc5bf579e707dea6942
wecgwm <wecgwm>
2024-02-16 18:01:30 +0800

    feat

--- src/main/java/com/wecgwm/sample/Application.java
+++ src/main/java/com/wecgwm/sample/Application.java
@@ -7,9 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
